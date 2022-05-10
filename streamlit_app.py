a29ad8e8b5a3ae89eaf133f2d70c08fbd762d837
iampmk <101439422+iampmk@users.noreply.github.com>
2022-05-10 16:29:40 -0400

    Update streamlit_app.py
    Added the menu and updated the title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title('My Parents Knew Healthy Diner')
+
+streamlit.title('My Mom\'s new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
