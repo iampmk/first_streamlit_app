7e2938537d18f5348c5dc498904ce36c06736ae0
iampmk <101439422+iampmk@users.noreply.github.com>
2022-05-10 16:44:12 -0400

    Update streamlit_app.py
    added default fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.multiselect('Pick some fruits: ',list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits: ',list(my_fruit_list.index),['Avacado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
 
