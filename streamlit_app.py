797c4cdd87a41f5f9b8a9ebc3759573a3cf61a5a
iampmk <101439422+iampmk@users.noreply.github.com>
2022-05-10 16:45:05 -0400

    Update streamlit_app.py
    fixed the typo in the fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.multiselect('Pick some fruits: ',list(my_fruit_list.index),['Avacado','Strawberries'])
+streamlit.multiselect('Pick some fruits: ',list(my_fruit_list.index),['Avocado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
 
